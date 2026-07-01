1207d1963b2a9caa678bde9d9651be0726cba852
Dev Dagar <ddagar321@gmail.com>
2026-07-01 14:24:05 +0530

    update

--- Ems-backend/index.js
+++ Ems-backend/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose"); 
 const employeeRoutes = require("./routes/employeeRoutes");
 const loggerMiddleware = require("./middleware/loggerMiddleware");
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 // Middleware
-
 app.use(express.json());
 app.use(loggerMiddleware);
 app.use(cors({
@@ -22,6 +24,14 @@ app.get("/", (req, res) => {
 
   res.send("Employee Management API Running");
 
+});
+
+mongoose.connect("mongodb+srv://Dev:DAGAR@cluster0.bspqa60.mongodb.net/EMS")
+.then(() => {
+  console.log("Connected to MongoDB");
+})
+.catch((err) => {
+  console.log(err);
 });
 
 app.listen(5000, () => {
